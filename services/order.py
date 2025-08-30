d7b9a66e75c76a7852f96ecf8d959613cc8a94ef
JulianskyMe <juliakamynskaya@gmail.com>
2025-08-30 15:15:27 +0300

    fix 02

--- services/order.py
+++ services/order.py
@@ -2,20 +2,33 @@ from django.db import transaction
 from django.contrib.auth import get_user_model
 from db.models import Order
 from django.db.models import QuerySet
+from datetime import datetime
+from db.models import Ticket
 
 
 User = get_user_model()
 
 @transaction.atomic
-def create_order(tickets: list, username: str, date: str | None) -> Order:
-    user = user.objects.get(username=username)
-    date_input = date
-    if date is None:
-        from django.utils import timezone
-        date_input = timezone.now()
-    order = Order.objects.create(user__username=username, created_at=date_input)
-    order.tickets.set(tickets)
+
+def create_order(tickets: list, username: str, date: str | None = None) -> Order:
+    user = get_user_model().objects.get(username=username)
+    created_at = datetime.strptime(date, "%Y-%m-%d %H:%M") if date else datetime.now()
+    order = Order.objects.create(user=user, created_at=created_at)
+
+    ticket_objs = []
+    for ticket in tickets:
+        ticket_obj = Ticket(
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session__id"],
+        )
+        ticket_objs.append(ticket_obj)
+
+    Ticket.objects.bulk_create(ticket_objs)
+
     return order
+
 
 
 def get_orders(username: str | None) -> QuerySet[Order]:
--- services/user.py
+++ services/user.py
@@ -1,24 +1,9 @@
-from db.models import User
-
-def create_user(
-    username: str, 
-    password: str, 
-    email: str | None, 
-    first_name: str | None, 
-    last_name: str | None
-    ) -> User:
-    user = User.objects.create_user(
-        username=username,
-        password=password,
-        email=email,
-        first_name=first_name,
-        last_name=last_name,
-    )
-    return user
+from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
 
 
-def get_user(user_id: int) -> User:
-    return User.objects.get(id=user_id)
+def get_user(user_id: int) -> QuerySet:
+    return get_user_model().objects.get(id=user_id)
 
 
 def update_user(
@@ -28,8 +13,8 @@ def update_user(
     email: str | None = None,
     first_name: str | None = None,
     last_name: str | None = None,
-) -> User:
-    user = User.objects.get(id=user_id)
+) -> QuerySet:
+    user = get_user(user_id)
 
     if username:
         user.username = username
@@ -42,5 +27,26 @@ def update_user(
     if last_name:
         user.last_name = last_name
 
+    user.save()
+    return user
+
+
+def create_user(
+    username: str, 
+    password: str, 
+    email: str | None, 
+    first_name: str | None, 
+    last_name: str | None
+    ) -> QuerySet:
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password
+        )
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
     user.save()
     return user
